0cfe28a845aa60f46640639c5b2ac8abce49dab0
Sobirjon Ismatov <sobirjonismatov70@gmail.com>
2024-08-09 01:23:38 +0500

    Fix routing issues for react-router-dom v6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/HomePage/Home";
 import Dashboard from "./pages/DashboardPage/Dashboard";
 import AddProduct from "./pages/AddProductPage/AddProduct";
-
 
 function App() {
   return (
@@ -12,6 +11,7 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/add" element={<AddProduct />} />
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" />} /> {/* Redirect to home for unmatched routes */}
       </Routes>
     </Router>
   );
